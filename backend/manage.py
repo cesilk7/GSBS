e70e02348421e5be1c42b852eef1c2299c0c5f12
ryuyax7 <2002ryuyax@gmail.com>
2021-08-30 22:09:32 +0900

    To be albe to boot with gunicorn

--- backend/manage.py
+++ backend/manage.py
@@ -8,18 +8,6 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-    # >>>>> added from here
-    if use_ptvsd():
-        if (os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN')) or \
-            os.environ.get('SHELL_WITH_PTVSD'):
-            import ptvsd
-            ptvsd.enable_attach(address=('0.0.0.0', 8888))
-            print(os.environ.get('RUN_MAIN'))
-            print(os.environ.get('WERKZEUG_RUN_MAIN'))
-            print(os.environ.get('SHELL_WITH_PTVSD'))
-            print('NOTICE: Attach ptvsd')
-    # <<<<< added to here
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -29,11 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-def use_ptvsd():
-    print(os.environ.get('USE_PTVSD'))
-    return os.environ.get('USE_PTVSD') == 'TRUE'
 
 
 if __name__ == '__main__':
