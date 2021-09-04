2c04c8532d9ce16c5823395fa7b3cc1eff8df5ce
ryuyax7 <2002ryuyax@gmail.com>
2021-09-04 14:18:57 +0900

    Setting static files

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -25,3 +25,4 @@ urlpatterns = [
     path('authen/', include('djoser.urls.jwt')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
